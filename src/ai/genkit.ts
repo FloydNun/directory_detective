5cb0c48c70dc295041231b6022f936bc15979462
Gary Lee <looseminds@gmail.com>
2026-01-18 11:27:33 +0000

    Try fixing this error: `Console GenkitError: UNKNOWN: Error fetching fro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,11 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      auth: {
+        type: 'googleCloud',
+      },
+    }),
+  ],
 });
